6f2f5980d53d34a3ef0f564a6739adc38cebcd79
danmaraba <danielkmaraba@gmail.com>
2024-03-14 12:39:15 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers=function(){
+    let newDrivers=drivers.slice(0,2)
+    return newDrivers;
+}
+
+const returnLastTwoDrivers=function(){
+    let lastTwoDrivers=drivers.slice(2);
+    return lastTwoDrivers;
+}
+
+const selectingDrivers=function(){
+    
+}
